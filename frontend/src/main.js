eee21054f2e66d18876efe32a20497ecca7a64c8
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2020-04-13 16:48:34 -0300

    Conclusão da página de cadastro de Categorias

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 
 // Token incorporado para qualquer requisição que o axios fizer
 // TEMPORARIO!
-require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6Ikx1Y2FzIERhbWFzY2VubyIsImVtYWlsIjoibHVjYXNkYW1hc2Nlbm9AZ21haWwuY29tIiwiYWRtaW4iOiJ0cnVlIiwiaWF0IjoxNTg2MjEyODgxLCJleHAiOjE1ODY0NzIwODF9.wQXhR6NRblDG1S-0DM36X1kZN1F5z7-G5dO3DNDUNRs'
+require('axios').defaults.headers.common['Authorization'] = 'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MywibmFtZSI6Ikx1Y2FzIERhbWFzY2VubyIsImVtYWlsIjoibHVjYXNkYW1hc2Nlbm9AZ21haWwuY29tIiwiYWRtaW4iOiJ0cnVlIiwiaWF0IjoxNTg2ODA0MDA2LCJleHAiOjE1ODcwNjMyMDZ9.6tk-Sh4SBBRiqeiXn7SVH6R92OBBescg18RjqcF9rlw'
 
 new Vue({
   store,
