bbc448eec56d10615882d21f128df353876c4a2b
Lucas Damasceno <54273070+lucasdam@users.noreply.github.com>
2020-04-15 20:52:24 -0300

    Conclusão da listagem de produtos por categoria e Exibição do produto

---
+++ frontend/src/components/product/ProductById.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="product-by-id">
+        <PageTitle icon="fa fa-file-o" :main="product.name" :sub="product.description" />
+        <div class="product-content" v-html="product.content"></div>
+    </div>
+</template>
+
+<script>
+import { baseApiUrl } from '@/global'
+import axios from 'axios'
+import PageTitle from '../template/PageTitle'
+
+export default {
+    name: 'ProductById',
+    components: { PageTitle },
+    data: function() {
+        return {
+            product: {}
+        }
+    },
+    mounted() {
+        const url = `${baseApiUrl}/products/${this.$route.params.id}`
+        axios.get(url).then(res => this.product = res.data)
+    }
+}
+</script>
+
+<style>
+    .product-content {
+        background-color: #FFF;
+        border-radius: 8px;
+        padding: 25px;
+    }
+
+    .product-content pre {
+        padding: 20px;
+        border-radius: 8px;
+        font-size: 1.2rem;
+        background-color: #1e1e1e;
+        color: #FFF;
+    }
+
+    .product-content img {
+        max-width: 100%;
+    }
+
+    .product-content :last-child {
+        margin-bottom: 0px;
+    }
+</style>
--- frontend/src/config/router.js
+++ frontend/src/config/router.js
@@ -5,6 +5,8 @@ import VueRouter from 'vue-router'
 
 import Home from '@/components/home/Home'
 import AdminPages from '@/components/admin/AdminPages'
+import ProductsByCategory from '@/components/product/ProductsByCategory'
+import ProductById from '@/components/product/ProductById'
 
 Vue.use(VueRouter)
 
@@ -17,6 +19,14 @@ const routes = [{
     name:'adminPages',
     path: '/admin',
     component: AdminPages
+}, {
+    name: 'productsByCategory',
+    path: '/categories/:id/products',
+    component: ProductsByCategory 
+}, {
+    name: 'productById',
+    path: '/products/:id',
+    component: ProductById
 }]
 
 // Instanciando e exportando o Vue Router
